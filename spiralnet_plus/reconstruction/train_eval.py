b84f96bab1e9bf317cc4954ad99de8ce43037fa1
tsingmessage <33644472+tsingmessage@users.noreply.github.com>
2020-11-18 12:15:42 +0100

    Update train_eval.py

--- spiralnet_plus/reconstruction/train_eval.py
+++ spiralnet_plus/reconstruction/train_eval.py
@@ -4,8 +4,6 @@ import torch
 import torch.nn.functional as F
 from psbody.mesh import Mesh
 import numpy as np
-import seaborn as sns
-import umap
 import matplotlib.pyplot as plt
 
 def run(model, train_loader, test_loader, epochs, optimizer, scheduler, writer,
@@ -62,7 +60,7 @@ def train(model, optimizer, loader, device,numz):
     for data in loader:
         optimizer.zero_grad()
         x = data.x.to(device)
-        out, gender, bz, gz,wz,hz, z ,_,bmi,wei,hei= model(data)
+        out = model(data)
         loss = F.l1_loss(out, x, reduction='mean') 
         loss.backward()
         total_loss += loss.item()
@@ -108,7 +106,7 @@ def test(model, loader, device, numz):
         
     with torch.no_grad():
         for i, data in enumerate(loader):
-            pred, gender,bz,gz ,wz,hz,z, gender_z,bmi,wei,hei= model(data)
+            pred = model(data)
             x = data.x.to(device)
             loss = F.l1_loss(pred, x, reduction='mean')
             total_loss += loss.item()
@@ -116,48 +114,3 @@ def test(model, loader, device, numz):
     return total_loss / len(loader)
 
 
-def eval_error(model, test_loader, device, meshdata, out_dir, mesh):
-    model.eval()
-
-    errors = []
-    mean = meshdata.mean
-    std = meshdata.std
-    with torch.no_grad():
-        for i, data in enumerate(test_loader):
-            x = data.x.to(device)
-            pred, _, _, _,_,_ ,_= model(data)
-            num_graphs = data.num_graphs
-            reshaped_pred = (pred.view(num_graphs, -1, 3).cpu() * std) + mean
-            reshaped_x = (x.view(num_graphs, -1, 3).cpu() * std) + mean
-            reshaped_pred_n = pred.cpu().numpy() * std.numpy() + mean.numpy()
-            reshaped_x_n = x.cpu().numpy() * std.numpy() + mean.numpy()
-            reshaped_pred_n = np.squeeze(reshaped_pred_n)
-            reshaped_x_n = np.squeeze(reshaped_x_n)
-            #print(np.shape(reshaped_x_n))
-            if i % 200 == 0:
-                result_mesh = Mesh(v=reshaped_pred_n, f=mesh.f)
-                expected_mesh = Mesh(v=reshaped_x_n, f=mesh.f)
-                result_mesh.write_ply('resul'+str(i)+'.ply',ascii=True)
-                expected_mesh.write_ply('original'+str(i)+'.ply',ascii=True)
-
-
-            reshaped_pred *= 1000
-            reshaped_x *= 1000
-
-            tmp_error = torch.sqrt(
-                torch.sum((reshaped_pred - reshaped_x)**2,
-                          dim=2))  # [num_graphs, num_nodes]
-            errors.append(tmp_error)
-        new_errors = torch.cat(errors, dim=0)  # [n_total_graphs, num_nodes]
-
-        mean_error = new_errors.view((-1, )).mean()
-        std_error = new_errors.view((-1, )).std()
-        median_error = new_errors.view((-1, )).median()
-
-    message = 'Error: {:.3f}+{:.3f} | {:.3f}'.format(mean_error, std_error,
-                                                     median_error)
-
-    out_error_fp = out_dir + '/euc_errors.txt'
-    with open(out_error_fp, 'a') as log_file:
-        log_file.write('{:s}\n'.format(message))
-    print(message)
